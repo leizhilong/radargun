8ea786f5b945bbb8925a3f4a405a74a3676cc1f6
Lei Zhilong <leizhilong@me.com>
2013-07-02 16:52:04 +0800

    modify local test case

---
+++ framework/src/main/resources/script/local-low.sh
@@ -0,0 +1,3 @@
+rm *.out
+rm *.log
+./bin/local.sh -c ./conf/local-benchmark-low-contention.xml
--- framework/src/main/resources/script/runl.sh
+++
@@ -1,5 +0,0 @@
-rm *.out
-rm *.log
-./bin/local.sh -c ./conf/local-benchmark.xml
-sleep 1
-tail -f radargun.log
